3d3b2d751ce7e49201c0c05ec6de8a6200802068
johnray786 <rehmanjuned1@gmail.com>
2023-09-28 20:05:12 +0500

    classes practice

--- class10.js
+++ class10.js
@@ -20,19 +20,50 @@
 // let abab = greet("Alex",false);
 // console.log(abab)
 // OBJECT ORIENTED PROGRAMMING
-class Student {
-    constructor(name, age) {
-        this.age = age;
-        this.name = name;
-        // console.log(this)
-        //this creates the object when you call new Student();
+// class Student {
+// name:string;
+// age:number;
+// constructor(name:string,age:number){
+//     this.age=age;
+//     this.name=name;
+//     // console.log(this)
+//     //this creates the object when you call new Student();
+// }
+// }
+// let studentInfo = new Student("Hamza",33);
+// console.log(studentInfo);
+// let studentInfo1 = new Student('Dilawar',22);
+// console.log(studentInfo1);
+// class Point {
+//     x!:number;
+// }
+// let bbbb = new Point();
+// bbbb.x=786;
+// class GoodGreeter {
+//     readonly name: string;//readonly does not allow change after first value assignment
+//     constructor(name:string){
+//         this.name=name;
+//     }
+//     greet(){
+//         //this.name='Arham' this will show error after readonly
+//         console.log(`Hello`,this.name);
+//     }
+// }
+// let myGreeter = new GoodGreeter(`Dilawar`);
+// // myGreeter.name = 'Ali'; this will also show error after readonly
+// myGreeter.greet();
+class Pizza {
+    constructor(pizzaTaste, pizzaSize, pizzaTopping) {
+        this.taste = pizzaTaste;
+        this.size = pizzaSize;
+        this.topping = pizzaTopping;
+        function print() { console.log(`You selected ${pizzaSize} sized ${pizzaTaste} pizza with ${pizzaTopping} topping`); }
+        print();
     }
 }
-let studentInfo = new Student("Hamza", 33);
-console.log(studentInfo);
-let studentInfo1 = new Student('Dilawar', 22);
-console.log(studentInfo1);
-class Point {
+let myPizza = new Pizza('Tikka', 'Medium', 'Sausage');
+// myPizza.taste.push('Cheese');
+// console.log(myPizza.taste);
+// console.log(myPizza)
+class Cusine extends Pizza {
 }
-let bbbb = new Point();
-bbbb.x = 786;
